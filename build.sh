13ee6e117708e12b4426383c383b2d5c1b48994c
galacticwarrior448 <shanthoshravi2006@gmail.com>
2021-07-17 14:48:55 +0530

    update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 echo "Setting up Build Environment"
-
 export CROSS_COMPILE=~/android/toolchain/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
 export USE_CCACHE=1
-mkdir -p out
+
+echo "Cleaning up previous build"
+
+make clean
+make mrproper
+rm -rf build/ak3/Image
+rm -rf build/ak3/dtb.img
+rm -rf out
+rm -rf dtb
+
+echo "Creating Directories"
+mkdir out
+mkdir dtb
 
 echo "Building Kernel"
 
@@ -16,9 +27,12 @@ echo "Building dtb"
 mv out/arch/arm64/boot/dts/exynos7420-zeroflte_eur_open_0* dtb/
 scripts/dtbtool -o "build/ak3/" -d dtb/ -s 2048
 mv out/arch/arm64/boot/Image build/ak3/
+
 echo "Creating flashables..."
+
 cd build/ak3
 zip -r9 ../G920X-Optimized-v1.1.zip * -x README.md ../G920X-Optimized-v1.1.zip
+
 echo "Done, your flashable can be found at build/"
 
 
